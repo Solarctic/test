d1e0bed3d82d84fa31a6382a03e1a96aa4d08bfd
Pooh Sathiarujikanon <1922pooh@gmail.com>
2023-03-15 19:29:46 +0900

    Asteroid Recreation in Unity

---
+++ Asteroid/Assets/Script/Player.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public new Rigidbody2D rigidbody { get; private set; }
+    public Bullet bulletPrefab;
+
+    public float thrustSpeed = 1f;
+    public bool thrusting { get; private set; }
+
+    public float turnDirection { get; private set; } = 0f;
+    public float rotationSpeed = 0.1f;
+
+    public float respawnDelay = 3f;
+    public float respawnInvulnerability = 3f;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        thrusting = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow);
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            turnDirection = 1f;
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            turnDirection = -1f;
+        }
+        else
+        {
+            turnDirection = 0f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (thrusting)
+        {
+            rigidbody.AddForce(transform.up * thrustSpeed);
+        }
+
+        if (turnDirection != 0f)
+        {
+            rigidbody.AddTorque(rotationSpeed * turnDirection);
+        }
+    }
+
+    private void Shoot()
+    {
+        Bullet bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+        bullet.Project(transform.up);
+    }
+}
